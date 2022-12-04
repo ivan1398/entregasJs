9d7e1d03506695c625a2c8cba1cf22ef114454b9
Ivan Bonti <ivanbonti300@gmail.com>
2022-12-04 13:22:55 -0300

    corrigiendo detalles y planteando lo que va a ser mas adelante el manejo de fechas

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ let nombreUsuario = prompt("Bienvenido al sistema de inscripciones a cursos MEDU
 let entrada = prompt("Hola"+" "+nombreUsuario+"! "+"Ingrese el nombre del curso al que desee inscribirse, el id del mismo en caso de que lo sepa o escriba ESC para salir: ");
 elegirCurso(entrada);
 
+// // objetos 
+// function curso (id,nombre,fechaInicio,fechaFin) {
+//     this.id = id;
+//     this.nombre = nombre;
+//     this.fechaInicio = fechaInicio;
+//     this.fechaFin = fechaFin;
+// }
+// //instanciacion de las clases
+// curso7Regiones = new curso(1,"7 regiones",2/11/2022,3/12/2023);
+// cursoDolorCronico = new curso(1,"dolor cronico",2/11/2022,3/12/2023);
+// cursoAnesteciaLocal = new curso(1,"anestecia local",2/11/2022,3/12/2023);
+// cursoTratamientosArtroscopicos = new curso(1,"Tratamientos artroscopicos",2/11/2022,3/12/2023);
+
+// //variable global 
+// fechaActual = new Date();
+//este codigo comentado es para chequear que la fecha donde la persona se anote no sea mayor a la que inicio el curso
+
+//espacio para definir funciones
 function elegirCurso(entrada) {
     
     while (entrada != "ESC") {
