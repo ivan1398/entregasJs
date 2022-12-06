9a778c54481c5366f36611ae26413d9b0273d60e
Ivan Bonti <ivanbonti300@gmail.com>
2022-12-06 10:29:08 -0300

    limpiando codigo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,8 @@
+document.write('<script src="funciones.js"></script>');
+
 let nombreUsuario = prompt("Bienvenido al sistema de inscripciones a cursos MEDU, ingrese su nombre por favor: ");
-let entrada = prompt("Hola"+" "+nombreUsuario+"! "+"Ingrese el nombre del curso al que desee inscribirse, el id del mismo en caso de que lo sepa o escriba ESC para salir: ");
+let entrada = prompt("Hola"+" "+nombreUsuario+"! "+"Ingrese 1-7 regiones, 2-Dolor cronico, 3-Anestecia local, 4-Tratamientos artroscopicos escriba ESC para salir: ");
 elegirCurso(entrada);
-
-// // objetos 
-// function curso (id,nombre,fechaInicio,fechaFin) {
-//     this.id = id;
-//     this.nombre = nombre;
-//     this.fechaInicio = fechaInicio;
-//     this.fechaFin = fechaFin;
-// }
-// //instanciacion de las clases
-// curso7Regiones = new curso(1,"7 regiones",2/11/2022,3/12/2023);
-// cursoDolorCronico = new curso(1,"dolor cronico",2/11/2022,3/12/2023);
-// cursoAnesteciaLocal = new curso(1,"anestecia local",2/11/2022,3/12/2023);
-// cursoTratamientosArtroscopicos = new curso(1,"Tratamientos artroscopicos",2/11/2022,3/12/2023);
-
-// //variable global 
-// fechaActual = new Date();
-//este codigo comentado es para chequear que la fecha donde la persona se anote no sea mayor a la que inicio el curso
 
 //espacio para definir funciones
 function elegirCurso(entrada) {
@@ -25,28 +10,28 @@ function elegirCurso(entrada) {
     while (entrada != "ESC") {
 
         switch(entrada) {
-            case "7 regiones": 
+            case "1": 
                 alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de 7 regiones: ");
                 abrirFormulario();
                 despedirUsuario(nombreUsuario);
                 entrada = prompt("Escriba ESC para salir ");
                 break;
 
-            case "Dolor cronico":
+            case "2":
                 alert(+nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de dolor cronico: ");
                 abrirFormulario();
                 despedirUsuario(nombreUsuario);
                 entrada = prompt("Escriba ESC para salir ");
                 break;
            
-            case "Anestecia local":
+            case "3":
                 alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de Anestecia local: ");
                 abrirFormulario();
                 despedirUsuario(nombreUsuario);
                 entrada = prompt("Escriba ESC para salir ");
                 break;
             
-            case "Tratamientos artroscopicos":
+            case "4":
                 alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de tratamientos artroscopicos: ");
                 abrirFormulario();
                 despedirUsuario(nombreUsuario);
@@ -87,3 +72,5 @@ function evaluarNombre(nombre) {
 function despedirUsuario(nombreUsuario) {
     alert("Gracias por confiar en nosotros"+" "+nombreUsuario+" "+"le aseguramos que será una experiencia sumamente enriquecedora");
 }
+
+
