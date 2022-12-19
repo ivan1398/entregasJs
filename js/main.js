79ba68cf5b5507db40964da725f6062e9edd06b8
Ivan Bonti <ivanbonti300@gmail.com>
2022-12-19 18:22:11 -0300

    Falta corregir la funcion que evalua si tiene ecografo y puede viajar, ademas extender la funcionalidad a los demas casos en la funcion principal

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 //espacio para variables
 fechaActual = new Date();
+let clientePorPagar;
+let nuevoPago;
+let booleano;
+
 let nombreUsuario = prompt("Bienvenido al sistema de inscripciones a cursos MEDU, ingrese su nombre por favor: ");
 let entrada = prompt("Hola"+" "+nombreUsuario+"! "+"Ingrese 1 Para inscribirse en 'Curso introductorio eco dolor crónico' , Ingrese 2 Para inscribirse en 'Curso básico introductorio dolor y eco intervencionismo' Ingrese 3 Para inscribirse en 'XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica' o escriba ESC para salir: ");
 
 //declaracion de las clases
-class Cliente  {
-    constructor(nombre,edad,pais,especialidad) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.pais = pais;
-        this.especialidad = especialidad
-    }
-};
-
 class Curso  {
     constructor(nombre,modalidad,Tema,fechaInicio,fechaFin) {
         this.nombre = nombre;
@@ -38,15 +33,22 @@ class Inscripcion  {
     }
 };
 
-class Pago  {
-    constructor(importe,fecha){
-        this.importe = importe;
+class medioPago {
+    constructor(Cliente,numeroTarjeta,fecha,codSeguridad){
+        this.Cliente = Cliente;
+        this.numeroTarjeta = numeroTarjeta;
         this.fecha = fecha;
+        this.codSeguridad = codSeguridad;
     }
 };
 
-class Formulario {
-    constructor(dni,numeroTel,codPostal,medioDeConoc,tieneEcografo,puedeViajar){
+class Cliente {
+    constructor(nombre,otroNombre,apellido,pais,especialidad,dni,numeroTel,codPostal,medioDeConoc,tieneEcografo,puedeViajar){
+        this.nombre = nombre;
+        this.otroNombre = otroNombre;
+        this.apellido = apellido;
+        this.pais = pais;
+        this.especialidad = especialidad;
         this.dni = dni;
         this.numeroTel = numeroTel;
         this.codPostal = codPostal;
@@ -54,11 +56,12 @@ class Formulario {
         this.tieneEcografo = tieneEcografo;
         this.puedeViajar = puedeViajar;
     }
-}
+};
 
 //declaracion de arrays
 const cursos = [];
 const clientes = [];
+const pagos = [];
 
 //instanciaciones de los temas
 const temasCurso1 = new Tema("-Introducción a la anatomo fisiología y neurobioquímica del dolor-Clasificación de los dolores. Nociplasticidad -Dolor miofascial. Fascia -Eco intervencionismo en dolor crónico");
@@ -68,33 +71,37 @@ const temasCurso2 = new Tema("Primer módulo: -Introducción a la anatomo fisiol
 const temasCurso3 = new Tema("Módulo 1: -Conociendo el ecógrafo -Medicina regenerativa: targets frecuentes por región bajo ecografía. -Dolor: algoritmos de inclusión de los procedimientos. Pacientes.-Tecnología en el consultorio Módulo 2: Región ecográfica 1 –Columna lumbo–sacra. Miembro inferior. Plexo lumbar y nervios terminales. Módulo 3: Región ecográfica 2 – Regiones del cuello y maxilofacial. Columna cervical. Plexo braquial en región supraclavicular Módulo 4: Región ecográfica 3 – Hombro, mano y codo. Plexo braquial. Bloqueos en miembro superior. Módulo 5: Región ecográfica 4 - Cadera, pelvis y región del muslo. Módulo 6: Región ecográfica 5 – Rodilla, regiones de la pierna, tobillo y pie. Módulo 7: Región ecográfica 6 -Columna torácica, pared de tórax ecográfica y pared de abdomen. Región paravertebral y espacio Intercostal.");
 
 //instanciaciones de las clases curso
-const curso1 = new Curso("Curso introductorio eco dolor crónico","Clases en vivo on-line",temasCurso1,2023-02-28,2023-03-07);
+const curso1 = new Curso("Curso introductorio eco dolor crónico","Clases en vivo on-line",temasCurso1,new Date(2023,1,27),new Date(2023,02,06));
 
 const curso2 = new Curso("Curso básico introductorio dolor y eco intervencionismo","Clases en vivo on-line",temasCurso2,2023-02-28,2023-03-07);
 
-const curso3 = new Curso("XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica","Clases en vivo on-line",temasCurso3,2023-03-21,2023-06-03);
+const curso3 = new Curso("XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica","Clases en vivo on-line",temasCurso3,new Date(2023,03,21),new Date(2023,06,03));
 
 //pushear los cursos al array
 cursos.push(curso1);
 cursos.push(curso2);
 cursos.push(curso3);
 
+//llamada de la funcion
 elegirCurso(entrada);
+
+//definicion de funciones
 function elegirCurso(entrada) {
 
     while (entrada != "ESC") {
 
         switch(entrada) {
             case "1":
-                //metodo de busqueda para encontrar el curso
-                if (chequearFechas(fechaIncio7regiones) == false) {
+                const cursoEncontrado1 = cursos.find((cursoBuscado) => cursoBuscado.nombre === "Curso introductorio eco dolor crónico");
+                fechaInicioCurso1 = cursoEncontrado1.fechaInicio;
+                if (chequearFechas(fechaInicioCurso1) == false) {
                     alert("Lo sentimos pero el curso ya ha iniciado! Sera un placer tenerlo en la próxima edición!");
                     entrada = prompt("Escriba ESC para salir ");
                     break;
                 }
-                else if (chequearFechas(fechaIncio7regiones) == true) {
-                    alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de 7 regiones: ");
-                    abrirFormulario();
+                else if (chequearFechas(fechaInicioCurso1) == true) {
+                    alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso: "+cursoEncontrado1.nombre);
+                    archivarCliente(nombreUsuario);
                     despedirUsuario(nombreUsuario);
                     entrada = prompt("Escriba ESC para salir ");
                     break;
@@ -155,18 +162,6 @@ function elegirCurso(entrada) {
         }
     }
 
-function abrirFormulario () {
-    let otroNombre = prompt("Si tiene otro nombre puede ingresarlo o presione enter para saltear este paso");
-    let apellido = prompt("Ingrese su apellido: ");
-    let numeroTarjeta = prompt("Ingrese su numero de tarjeta con la que abonara el curso: ");
-    let codSeguridad = prompt("Ingrese el codigo de seguridad de la tarjeta: ");
-    alert("A continuacion le mostramos los datos ingresados para que chequee si son correctos o no: ");
-    alert("Su nombre de pila es: "+nombreUsuario);
-    evaluarNombre(otroNombre);
-    alert("Su apeliido es: "+apellido);
-    alert("El numero de su tarjeta es: "+numeroTarjeta);
-    alert("El codigo de seguridad de su tarjeta es: "+codSeguridad);
-}
 
 function evaluarNombre(nombre) {
 
@@ -176,7 +171,7 @@ function evaluarNombre(nombre) {
     else {
         alert("Su segundo nombre es: "+nombre);
     }
-}
+};
 
 function despedirUsuario(nombreUsuario) {
     alert("Gracias por confiar en nosotros"+" "+nombreUsuario+" "+"le aseguramos que será una experiencia sumamente enriquecedora");
@@ -190,7 +185,98 @@ function chequearFechas(unaFecha) {
         return true;
     }
 
-}
+};
+
+function archivarCliente(nombreUsuario) {
+    alert("A continuación le pediremos una serie de datos para poder inscribirlo al curso correctamente, por favor clickee en aceptar");
+    nombre = nombreUsuario;
+    let otroNombre = prompt("Si tiene otro nombre puede ingresarlo o presione enter para saltear este paso");
+    let apellido = prompt("Ingrese su apellido: ");
+    let pais = prompt("Ingrese su nacionalidad: ");
+    let especialidad = prompt("Ingrese su especialidad en la medicina: ");
+    let dni = prompt("Escriba su numero de documento de identidad: ");
+    let numeroTel = prompt("Proporcionenos un numero de telefono para contactarnos con usted: ");
+    let codPostal = prompt ("Ingrese su codigo postal: ");
+    let medioDeConoc = prompt("Ingrese el medio mediante el cual nos ha conocido: ");
+    let entradaEcografo = prompt("Ingrese 'sí' si usted posee un ecógrafo para trabajar, de lo contrario ingrese 'no'");
+    if (devuelveBoolean(entradaEcografo)){
+        tieneEcografo = true;
+    } else {
+        tieneEcografo = false;
+    };
+    let entradaViaje = prompt("Ingrese 'sí' si usted posee un ecógrafo para trabajar, de lo contrario ingrese 'no'");
+    if (devuelveBoolean(entradaViaje)){
+         puedeViajar = true;
+    } else {
+         puedeViajar = false;
+    };
+    clientePorPagar = new Cliente(nombre,otroNombre,apellido,pais,especialidad,dni,numeroTel,codPostal,medioDeConoc,tieneEcografo,puedeViajar);
+    clientes.push(clientePorPagar);
+    asignarPago(clientePorPagar);
+};
+
+
+function devuelveBoolean(entradaSiNo) {
+    if(entradaSiNo == "sí"){
+        return true;
+    } else{
+        return false;
+    }
+
+};
+
+function asignarPago(clientePorPagar) {
+    alert("Le pediremos a continuación una serie de datos para que pueda abonar el curso al cual acaba de inscribirse, por favor seleccione el botón aceptar");
+    numeroTarjeta = prompt("Ingrese por favor el numero de tarjeta");
+    fecha = new Date();
+    codSeguridad = prompt("Ingrese por favor el codigo de seguridad de su tarjeta: ");
+    nuevoPago = new medioPago(clientePorPagar,numeroTarjeta,fecha,codSeguridad);
+    pagos.push(nuevoPago);
+    mostrarDatos(clientePorPagar,nuevoPago);
+};
+
+function evaluarEcografo(booleano) {
+
+    if (booleano == "true"){
+        alert("Usted posee un ecógrafo propio para trabajar durante el curso");
+    }
+    else if (booleano == "false") {
+        alert("Usted no posee un ecógrafo propio para trabajar durante el curso");
+    }
+};
+
+function evaluarPuedeViajar(booleano) {
+    if (booleano == "true"){
+        alert("Usted podrá viajar en días acordados para rotar por nuestro centro en Argentina");
+    }
+    else if (booleano == "false") {
+        alert("Usted no podrá viajar en días acordados para rotar por nuestro centro en Argentina");
+    }  
+};
+
+function mostrarDatos(clientePorPagar,nuevoPago){
+    alert("A continuacion le mostraremos los datos proporcionados para chequear su veracidad, por favor seleccione aceptar");
+    alert("Su nombre de pila es: "+clientePorPagar.nombre);
+    evaluarNombre(clientePorPagar.otroNombre);
+    alert("Su apellido es: "+clientePorPagar.apellido);
+    alert("Usted es de: "+clientePorPagar.pais);
+    alert("Su especialidad en la medicina es: "+clientePorPagar.especialidad);
+    alert("Su dni es: "+clientePorPagar.dni);
+    alert("Su numero telefonico es: "+clientePorPagar.numeroTel);
+    alert("El código postal de su país de origen es: "+clientePorPagar.codPostal);
+    alert("Usted nos conoció mediante: "+clientePorPagar.medioDeConoc);
+    evaluarEcografo(clientePorPagar.tieneEcografo);
+    evaluarPuedeViajar(clientePorPagar.puedeViajar);
+    //pago
+    alert("El número de su tarjeta es: "+nuevoPago.numeroTarjeta);
+    alert("El código de seguridad de su tarjeta es: "+nuevoPago.codSeguridad);
+};
+
+
+
+
+
+
 
 
 
