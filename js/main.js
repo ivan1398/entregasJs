6ea43979ec58f6ba2fda8c7d88c58707346de60f
Ivan Bonti <ivanbonti300@gmail.com>
2022-12-20 02:09:54 -0300

    ya funciona cad una de las nuevas funciones y funcionalidad extendida a todos los cursos

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let nuevoPago;
 let booleano;
 
 let nombreUsuario = prompt("Bienvenido al sistema de inscripciones a cursos MEDU, ingrese su nombre por favor: ");
-let entrada = prompt("Hola"+" "+nombreUsuario+"! "+"Ingrese 1 Para inscribirse en 'Curso introductorio eco dolor crónico' , Ingrese 2 Para inscribirse en 'Curso básico introductorio dolor y eco intervencionismo' Ingrese 3 Para inscribirse en 'XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica' o escriba ESC para salir: ");
+let entrada = prompt("Hola"+" "+nombreUsuario+"! "+"Ingrese 1 Para inscribirse en 'Curso introductorio eco dolor crónico' , Ingrese '2' Para inscribirse en 'Curso básico introductorio dolor y eco intervencionismo' Ingrese '3' Para inscribirse en 'XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica' o escriba ESC para salir: ");
 
 //declaracion de las clases
 class Curso  {
@@ -73,7 +73,7 @@ const temasCurso3 = new Tema("Módulo 1: -Conociendo el ecógrafo -Medicina rege
 //instanciaciones de las clases curso
 const curso1 = new Curso("Curso introductorio eco dolor crónico","Clases en vivo on-line",temasCurso1,new Date(2023,1,27),new Date(2023,02,06));
 
-const curso2 = new Curso("Curso básico introductorio dolor y eco intervencionismo","Clases en vivo on-line",temasCurso2,2023-02-28,2023-03-07);
+const curso2 = new Curso("Curso básico introductorio dolor y eco intervencionismo","Clases en vivo on-line",temasCurso2,new Date(2023,02,28), new Date(2023,03,07));
 
 const curso3 = new Curso("XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica","Clases en vivo on-line",temasCurso3,new Date(2023,03,21),new Date(2023,06,03));
 
@@ -105,62 +105,45 @@ function elegirCurso(entrada) {
                     despedirUsuario(nombreUsuario);
                     entrada = prompt("Escriba ESC para salir ");
                     break;
-                }
-
-
+                };
             case "2":
-                fechaIncioDolorCronico = new Date(2023,12,30);
-                if (chequearFechas(fechaIncioDolorCronico) == false) {
+                const cursoEncontrado2 = cursos.find((cursoBuscado) => cursoBuscado.nombre === "Curso básico introductorio dolor y eco intervencionismo");
+                fechaInicioCurso2 = cursoEncontrado2.fechaInicio;
+                if (chequearFechas(fechaInicioCurso2) == false) {
                     alert("Lo sentimos pero el curso ya ha iniciado! Sera un placer tenerlo en la próxima edición!");
                     entrada = prompt("Escriba ESC para salir ");
                     break;
                 }
-                else {
-                    alert(+nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de dolor cronico: ");
-                    abrirFormulario();
+                else if (chequearFechas(fechaInicioCurso2) == true) {
+                    alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso: "+cursoEncontrado2.nombre);
+                    archivarCliente(nombreUsuario);
                     despedirUsuario(nombreUsuario);
                     entrada = prompt("Escriba ESC para salir ");
                     break;
-                }
-
+                };
             case "3":
-                fechaIncioAnesteciaLocal = new Date(2023,12,30);
-                if (chequearFechas(fechaIncioAnesteciaLocal) == false) {
+                const cursoEncontrado3 = cursos.find((cursoBuscado) => cursoBuscado.nombre === "XXII Curso MeDU de Ecografía  Intervencionista en dolor crónico, agudo y perianalgesia quirúrgica");
+                fechaInicioCurso3 = cursoEncontrado3.fechaInicio;
+                if (chequearFechas(fechaInicioCurso3) == false) {
                     alert("Lo sentimos pero el curso ya ha iniciado! Sera un placer tenerlo en la próxima edición!");
                     entrada = prompt("Escriba ESC para salir ");
                     break;
                 }
-                else {
-                    alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de Anestecia local: ");
-                    abrirFormulario();
+                else if (chequearFechas(fechaInicioCurso3) == true) {
+                    alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso: "+cursoEncontrado3.nombre);
+                    archivarCliente(nombreUsuario);
                     despedirUsuario(nombreUsuario);
                     entrada = prompt("Escriba ESC para salir ");
                     break;
-                }
-
-
-            case "4":
-                fechaInicioTratamientosArtroscopicos = new Date(2021,07,16);
-                if (chequearFechas(fechaInicioTratamientosArtroscopicos) == false) {
-                    alert("Lo sentimos pero el curso ya ha iniciado! Sera un placer tenerlo en la próxima edición!");
-                    entrada = prompt("Escriba ESC para salir ");
-                    break;
-                }
-                else {
-                    alert(nombreUsuario+" "+"seleccione aceptar y se abrira el formulario de inscripcion para el curso de tratamientos artroscopicos: ");
-                    abrirFormulario();
-                    despedirUsuario(nombreUsuario);
-                    entrada = prompt("Escriba ESC para salir ");
-                    break;
-                }
-
+                };
             default:
                 alert("Disculpe que no podamos ayudarlo! El curso que ingreso no se encuentra disponible momentaneamente");
                 entrada = prompt("Escriba ESC para salir ");
                 break;
-            }
-        }
-    }
+            };
+        };
+    };
+
 
 
 function evaluarNombre(nombre) {
@@ -190,39 +173,19 @@ function chequearFechas(unaFecha) {
 function archivarCliente(nombreUsuario) {
     alert("A continuación le pediremos una serie de datos para poder inscribirlo al curso correctamente, por favor clickee en aceptar");
     nombre = nombreUsuario;
-    let otroNombre = prompt("Si tiene otro nombre puede ingresarlo o presione enter para saltear este paso");
-    let apellido = prompt("Ingrese su apellido: ");
-    let pais = prompt("Ingrese su nacionalidad: ");
-    let especialidad = prompt("Ingrese su especialidad en la medicina: ");
-    let dni = prompt("Escriba su numero de documento de identidad: ");
-    let numeroTel = prompt("Proporcionenos un numero de telefono para contactarnos con usted: ");
-    let codPostal = prompt ("Ingrese su codigo postal: ");
-    let medioDeConoc = prompt("Ingrese el medio mediante el cual nos ha conocido: ");
-    let entradaEcografo = prompt("Ingrese 'sí' si usted posee un ecógrafo para trabajar, de lo contrario ingrese 'no'");
-    if (devuelveBoolean(entradaEcografo)){
-        tieneEcografo = true;
-    } else {
-        tieneEcografo = false;
-    };
-    let entradaViaje = prompt("Ingrese 'sí' si usted posee un ecógrafo para trabajar, de lo contrario ingrese 'no'");
-    if (devuelveBoolean(entradaViaje)){
-         puedeViajar = true;
-    } else {
-         puedeViajar = false;
-    };
+    otroNombre = prompt("Si tiene otro nombre puede ingresarlo o presione enter para saltear este paso");
+    apellido = prompt("Ingrese su apellido: ");
+    pais = prompt("Ingrese su nacionalidad: ");
+    especialidad = prompt("Ingrese su especialidad en la medicina: ");
+    dni = prompt("Escriba su numero de documento de identidad: ");
+    numeroTel = prompt("Proporcionenos un numero de telefono para contactarnos con usted: ");
+    codPostal = prompt ("Ingrese su codigo postal: ");
+    medioDeConoc = prompt("Ingrese el medio mediante el cual nos ha conocido: ");
+    tieneEcografo = prompt("Ingrese 'sí' si usted posee un ecógrafo para trabajar, de lo contrario ingrese 'no'");
+    puedeViajar = prompt("Ingrese 'sí' si usted puede viajar a nuestro centro en Concepción del Uruguay, Entre Ríos. Argentina en fechas acordadas previamente, de lo contrario ingrese 'no'");
     clientePorPagar = new Cliente(nombre,otroNombre,apellido,pais,especialidad,dni,numeroTel,codPostal,medioDeConoc,tieneEcografo,puedeViajar);
     clientes.push(clientePorPagar);
     asignarPago(clientePorPagar);
-};
-
-
-function devuelveBoolean(entradaSiNo) {
-    if(entradaSiNo == "sí"){
-        return true;
-    } else{
-        return false;
-    }
-
 };
 
 function asignarPago(clientePorPagar) {
@@ -235,23 +198,30 @@ function asignarPago(clientePorPagar) {
     mostrarDatos(clientePorPagar,nuevoPago);
 };
 
-function evaluarEcografo(booleano) {
-
-    if (booleano == "true"){
+function evaluarEcografo(tieneEcografo) {
+    tieneEcografo = tieneEcografo.toLowerCase();
+    if (tieneEcografo === "sí" || tieneEcografo === "si"){
         alert("Usted posee un ecógrafo propio para trabajar durante el curso");
     }
-    else if (booleano == "false") {
+    else if (tieneEcografo === "no"){
         alert("Usted no posee un ecógrafo propio para trabajar durante el curso");
+    }
+    else {
+        alert("Usted no ha especificado correctamente si tiene o no un ecógrafo, por favor vuelva a ingresar y especifique una de las opciones proporcionadas");
     }
 };
 
-function evaluarPuedeViajar(booleano) {
-    if (booleano == "true"){
+function evaluarPuedeViajar(puedeViajar) {
+    puedeViajar =  puedeViajar.toLowerCase();
+    if (puedeViajar === "sí" || tieneEcografo === "si"){
         alert("Usted podrá viajar en días acordados para rotar por nuestro centro en Argentina");
     }
-    else if (booleano == "false") {
-        alert("Usted no podrá viajar en días acordados para rotar por nuestro centro en Argentina");
+    else if (puedeViajar === "no") {
+        alert("Usted no podrá viajar a rotar por nuestro centro en Argentina");
     }  
+    else {
+        alert("Usted no ha especificado correctamente si puede o no viajar, por favor vuelva a ingresar y especifique una de las opciones proporcionadas");
+    }
 };
 
 function mostrarDatos(clientePorPagar,nuevoPago){
